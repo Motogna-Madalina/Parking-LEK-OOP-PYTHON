14fbad053713d436095d1ea9377e0500c5bd6b25
Madalina Motogna <054093@edu.szf.at>
2026-06-19 00:25:24 +0200

    Version final

--- Parking- calculator/fahrzeug.py
+++ Parking- calculator/fahrzeug.py
@@ -15,12 +15,8 @@ class Fahrzeug:
 
         if fahrzeugtyp.upper() not in fest_faktoren:
             raise ValueError(
-<<<<<<< HEAD
-                "Fahrzeugtyp ist nicht gefunden.\nBitte schreiben Sie: MOTORRAD, PKW oder TRANSPORT"
-=======
-                    "Unbekannter Fahrzeugtyp.\nBitte geben Sie einen der folgenden Fahrzeugtypen ein: MOTORRAD, PKW oder TRANSPORT."
+                "Unbekannter Fahrzeugtyp.\nBitte geben Sie einen der folgenden Fahrzeugtypen ein: MOTORRAD, PKW oder TRANSPORT."
 
->>>>>>> df5c8c0f9e8ad1d86fbe6e3263e6e83b132463e2
             )
 
         return fest_faktoren[fahrzeugtyp.upper()]
--- Parking- calculator/menu.py
+++ Parking- calculator/menu.py
@@ -11,11 +11,8 @@ def start_program():
         print("1 - Fahrzeug erfassen")
         print("2 - Tarife anzeigen")
         print("3 - Parkpreis berechnen")
-<<<<<<< HEAD
         print("4 - Parkticket drucken")
-=======
         print("5 - Parkticket drucken")
->>>>>>> df5c8c0f9e8ad1d86fbe6e3263e6e83b132463e2
         print("Q - Beenden")
 
         choice = input("Select: ").upper()
@@ -29,16 +26,12 @@ def start_program():
         elif choice == "3":
             manager.parkpreis_berechnen()
 
-<<<<<<< HEAD
         elif choice == "4":
             manager.parkticket_drucken()
 
-
-=======
         elif choice == "5":
             manager.parkticket_drucken()
 
->>>>>>> df5c8c0f9e8ad1d86fbe6e3263e6e83b132463e2
         elif choice == "Q":
             print("Beenden.")
             break
